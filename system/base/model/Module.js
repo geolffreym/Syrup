e54c5fa7284cd8a8713f1441199bbae09d74cffa
gmena <gmjun2000@yahoo.com>
2014-12-24 12:54:41 -0600

    Give your JS App MVC | MTV pattern with Models, Views, Controllers, Templates, Events and Connections ->Auto serve template

--- system/base/model/Module.js
+++ system/base/model/Module.js
@@ -179,6 +179,7 @@ Module.add ( '_taste', function ( moduleId, event ) {
 		if ( !_.isSet ( event ) ) {
 			_self._add ( moduleId );
 			_self.modules[moduleId].instance = _self._trigger ( moduleId );
+			_self.modules[moduleId].instance.name = moduleId;
 			_self.modules[moduleId].instance.dom = _$ ( '[syrup-event="' + moduleId + '"]+' );
 			_self.modules[moduleId].instance.template = _.isSet ( _self.modules[moduleId].instance.template );
 			_self.modules[moduleId].instance.on = function ( event, callback ) {
