c51783a97a0ddb042ebd9a80fc9364eea7aaa81c
gmena <gmjun2000@gmail.com>
2015-10-13 10:24:30 -0600

    MiddleWare

---
+++ system/base/MiddleWare.js
@@ -0,0 +1,13 @@
+/**
+ * Created by gmena on 10-13-15.
+ */
+
+(function (window) {
+	"use strict";
+	function MiddleWare () {
+
+	}
+
+	window.MiddleWare = new MiddleWare;
+	window.MiddleWareClass = MiddleWare;
+}) (window);
