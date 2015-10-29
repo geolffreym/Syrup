0749e80cc3eb5f7528b43e04238886100c231b8f
gmena <gmjun2000@gmail.com>
2015-10-29 10:41:15 -0600

    * Router uri fix

--- system/base/Router.js
+++ system/base/Router.js
@@ -15,7 +15,7 @@
 	/**Router
 	 * @constructor
 	 */
-	function Router () {
+	function Router() {
 		this.routes = {};
 		this.history = window.history;
 		this.cleanParam = /([\w]+:)/g;
@@ -28,17 +28,17 @@
 		var _self = this;
 
 		//Set Pop State
-		window.addEventListener ('popstate', function (e) {
+		window.addEventListener('popstate', function (e) {
 			//Intercept pop state
-			_self._handleInterceptor ('popstate', e);
+			_self._handleInterceptor('popstate', e);
 
-			if ( _.isSet (e.state) && 'route_name' in e.state ) {
-				if ( e.state.route_name in _self.onpopstate ) {
-					_.each (_self.onpopstate[e.state.route_name], function (v, i) {
-						v (e.state, e);
+			if (_.isSet(e.state) && 'route_name' in e.state) {
+				if (e.state.route_name in _self.onpopstate) {
+					_.each(_self.onpopstate[e.state.route_name], function (v, i) {
+						v(e.state, e);
 
 						//Intercept pop state
-						_self._handleInterceptor ('redirect', e);
+						_self._handleInterceptor('redirect', e);
 					}, true);
 				}
 			}
@@ -50,9 +50,9 @@
 	 * @param {object} routes
 	 * @return {object}
 	 * */
-	Router.add ('connect', function (to_route) {
-		if ( !(to_route instanceof AppClass) )
-			_.error (WARNING_ROUTE.ERROR.BADINSTANCE, '(Router .route)');
+	Router.add('connect', function (to_route) {
+		if (!(to_route instanceof AppClass))
+			_.error(WARNING_ROUTE.ERROR.BADINSTANCE, '(Router .route)');
 
 		this.module = to_route;
 		to_route.lazy = true;
@@ -63,12 +63,12 @@
 	 * @param {object} routes
 	 * @return {object}
 	 * */
-	Router.add ('set', function (routes) {
+	Router.add('set', function (routes) {
 		var _self = this;
 
-		return (new Promise (function (resolve, reject) {
-			_self.routes = _.extend (_self.routes, routes);
-			resolve (_self);
+		return (new Promise(function (resolve, reject) {
+			_self.routes = _.extend(_self.routes, routes);
+			resolve(_self);
 		}))
 	});
 
@@ -77,55 +77,77 @@
 	 * @param {function} callback
 	 * @return {void}
 	 */
-	Router.add ('_handleSkull', function (conf, callback, params) {
+	Router.add('_handleSkull', function (conf, callback, params) {
 		var _view = new View;
 
 		//Clear cache?
-		if ( !conf.cache )
-			_view.cleanCache (conf.tpl);
+		if (!conf.cache)
+			_view.cleanCache(conf.tpl);
 
 		//Get the tpl skull
-		_view.seekTpl (conf.tpl).then (function (view) {
+		_view.seekTpl(conf.tpl).then(function (view) {
 
-			// Find main app dom container...!!!
-			var _main = _$ ('[sp-app]');
-
+			// Find main
+			var _main = _$('[sp-app]');
 			// Exist the skull?
-			if ( _main.exist )
-				_main.html (view.getTpl ());
+			if (_main.exist)
+				_main.html(view.getTpl());
 
 			//Execute
-			callback.apply (conf.app, params);
+			callback.apply(conf.app, params);
 		});
 	});
 
 	/** Routing
-	 * @param {route_name} string
+	 * @param {string} route_name
 	 * @return {void}
 	 */
-	Router.add ('_route', function (route_name) {
+	Router.add('_route', function (route_name) {
 		var _the_route = this.routes[route_name],
-			_query_params = _.queryStringToJson (location.search),
-			_uri_path = this._rewriteRules (_the_route, _query_params) || location.pathname,
-			_uri_path_slash_index = _.oChars (_uri_path, '/'),
-			_the_route_slash_index = _.oChars (_the_route, '/');
+			_query_params = _.queryStringToJson(location.search),
+			_uri_path = this._rewriteRules(_the_route, _query_params) || location.pathname,
+			_uri_path_slash_index = _.oChars(_uri_path, '/'),
+			_the_route_slash_index = _.oChars(_the_route, '/');
 
 		//Clean param from route
-		_the_route = _.replace (_the_route, this.cleanParam, _.emptyStr);
+		_the_route = this._cleanedUriParams(_the_route);
 
 		//In route '/' slash not needed at end
-		if ( _uri_path_slash_index > _the_route_slash_index
+		if (_uri_path_slash_index > _the_route_slash_index
 		) {
 			//Remove last slash from pathname
-			_uri_path = _.truncateString (_uri_path, -1);
-		} else if ( _the_route_slash_index > _uri_path_slash_index ) {
+			_uri_path = _.truncateString(_uri_path, -1);
+		} else if (_the_route_slash_index > _uri_path_slash_index) {
 			//In route '/' slash needed at end
 			//Append slash to route
 			_uri_path += '/';
 		}
 
-		return (new RegExp ('^' + _the_route + '$').test (_uri_path))
-			? { uri: _uri_path, route: _the_route, query: _query_params } : false;
+		return this._matchUri(_the_route, _uri_path) ? {
+			uri: _uri_path,
+			route: _the_route,
+			query: _query_params,
+			match: true
+		} : false;
+	});
+
+	/** Match the uri
+	 * @param {string} _the_route
+	 * @param {string} _uri_path
+	 * @return {string}
+	 */
+	Router.add('_matchUri', function (_the_route, _uri_path) {
+		//Clean param from route
+		return (new RegExp('^' + _the_route + '$').exec(_uri_path))
+	});
+
+	/** Clean params from RegExp
+	 * @param {string} _the_route
+	 * @return {string}
+	 */
+	Router.add('_cleanedUriParams', function (_the_route) {
+		//Clean param from route
+		return _.replace(_the_route, this.cleanParam, _.emptyStr);
 	});
 
 	/**Delegate routes
@@ -133,8 +155,8 @@
 	 * @param {object} conf
 	 * @returns {object}
 	 */
-	Router.add ('when', function (route_name, conf) {
-		_.assert (route_name, _.WARNING_SYRUP.ERROR.NOPARAM, '(Router .when)');
+	Router.add('when', function (route_name, conf) {
+		_.assert(route_name, _.WARNING_SYRUP.ERROR.NOPARAM, '(Router .when)');
 		var _self = this,
 			_router;
 
@@ -146,29 +168,29 @@
 			return _self;
 
 		//No route?
-		if ( !(route_name in _self.onpopstate) )
+		if (!(route_name in _self.onpopstate))
 			_self.onpopstate[route_name] = [];
 
 		//Append a new route
-		_self.onpopstate[route_name].push (function (state, e) {
+		_self.onpopstate[route_name].push(function (state, e) {
 			//Handle tpl?
-			_self._handleSkull (conf, function () {
-
+			_self._handleSkull(conf, function () {
 				//On main tpl is handled, what to do?
-				if ( conf.app in _self.module.appCollection ) {
+
+				if (conf.app in _self.module.appCollection) {
 					//Intercept init
 					//Inject params
-					_self.module.appCollection[conf.app].intercept ({
+					_self.module.appCollection[conf.app].intercept({
 						'init': function (mod) {
 							mod.uri = {
 								params: state,
-								route : _self.routes[route_name]
+								route: _self.routes[route_name]
 							}
 						}
 					});
 
 					//Taste recipes
-					_self.module.appCollection[conf.app].taste ();
+					_self.module.appCollection[conf.app].taste();
 				}
 
 			}, [state, e])
@@ -177,10 +199,10 @@
 
 		//First action
 		//Routing!!!
-		if ( (_router = _self._route (route_name)) ) {
+		if ((_router = _self._route(route_name))) {
 			//No default
 			_self.default = false;
-			_self.redirect (route_name, _router.query);
+			_self.redirect(route_name, _router.query, _router);
 		}
 
 		return _self;
@@ -191,8 +213,8 @@
 	 * @param {string} route_name
 	 * @return {object}
 	 * */
-	Router.add ('redirect', function (route_name, params, config) {
-		_.assert (route_name, _.WARNING_SYRUP.ERROR.NOPARAM, '(Router .redirect)');
+	Router.add('redirect', function (route_name, params, config) {
+		_.assert(route_name, _.WARNING_SYRUP.ERROR.NOPARAM, '(Router .redirect)');
 
 		var _self = this,
 			_the_new_route = null,
@@ -201,34 +223,34 @@
 			};
 
 		//Redirect
-		return (new Promise (function (resolve, reject) {
+		return (new Promise(function (resolve, reject) {
 
 			//Not routing
-			if ( !(route_name in _self.routes) ) {
-				reject (route_name);
+			if (!(route_name in _self.routes)) {
+				reject(route_name);
 				return;
 			}
 
-
 			//Params and config
-			_params = _.isObject (params) && params || {};
-			_config = _.extend (_config, config || {}, true);
+			_params = _.isObject(params) && params || {};
+			_config = _.extend(_config, config || {}, true);
 
 			//Set old regex in state object
 			_the_new_route = _self.routes[route_name];
 
 			//Rewrite rules or remove regexp params
-			_the_new_route = _self._rewriteRules (_the_new_route, _params) ||
-							 _.replace (_the_new_route, _self.findParams, _.emptyStr);
+			_the_new_route = _self._rewriteRules(_the_new_route, _params)
+							 || 'uri' in _config && _config.uri
+							 || _the_new_route;
 
 			//Set state in history
-			_self._triggerPopState (
-				_.extend (_params, { 'route_name': route_name }),
+			_self._triggerPopState(
+				_.extend(_params, {'route_name': route_name}),
 				route_name, _the_new_route, _config
 			);
 
 			//Resolve Promise
-			resolve (_the_new_route);
+			resolve(_the_new_route);
 
 		}));
 	});
@@ -237,14 +259,14 @@
 	 * @param  {object} interceptors
 	 * @return {object}
 	 * */
-	Router.add ('otherwise', function (route_name, conf) {
+	Router.add('otherwise', function (route_name, conf) {
 		if (
-			_.isString (route_name)
+			_.isString(route_name)
 			&& route_name in this.routes
 			&& this.default
 		) {
-			this.when (route_name, conf);
-			this.redirect (route_name, {});
+			this.when(route_name, conf);
+			this.redirect(route_name, {});
 		}
 
 		return this;
@@ -254,9 +276,9 @@
 	 * @param  {object} interceptors
 	 * @return {object}
 	 * */
-	Router.add ('intercept', function (interceptors) {
-		if ( _.isObject (interceptors) )
-			MiddleWare.intercept (this, interceptors);
+	Router.add('intercept', function (interceptors) {
+		if (_.isObject(interceptors))
+			MiddleWare.intercept(this, interceptors);
 		return this;
 	});
 
@@ -266,15 +288,15 @@
 	 * @param {object} param
 	 * @return {void}
 	 * */
-	Router.add ('_handleInterceptor', function (type, param) {
+	Router.add('_handleInterceptor', function (type, param) {
 		//Trigger Interceptors
-		MiddleWare.trigger (
-			MiddleWare.getInterceptors (this, type),
+		MiddleWare.trigger(
+			MiddleWare.getInterceptors(this, type),
 			[param, this]
 		);
 
 		//Clean the interceptor
-		MiddleWare.cleanInterceptor (this, type);
+		MiddleWare.cleanInterceptor(this, type);
 	});
 
 	/** Uri rewrite
@@ -282,22 +304,22 @@
 	 * @param {object} _params
 	 * @return {void}
 	 * */
-	Router.add ('_rewriteRules', function (_the_new_route, _params) {
+	Router.add('_rewriteRules', function (_the_new_route, _params) {
 
 		//Exists params?
 		//Rewrite rules uri
 		//Get the params groups
 		var _findParams = this.findParams,
-			_get_groups = _.getObjectSize (_params) > 0 && _.getRegExpGroup (
-					_the_new_route, _findParams, _.getObjectKeys (_params)
+			_get_groups = _.getObjectSize(_params) > 0 && _.getRegExpGroup(
+					_the_new_route, _findParams, _.getObjectKeys(_params)
 				);
 
 		//Rewrite URL
-		return _get_groups && _.replace (
+		return _get_groups && _.replace(
 				_the_new_route, _findParams,
-				_.toObject (
-					_.getObjectValues (_get_groups),
-					_.getObjectValues (_params)
+				_.toObject(
+					_.getObjectValues(_get_groups),
+					_.getObjectValues(_params)
 				)
 			) || false;
 
@@ -310,14 +332,14 @@
 	 * @param {string} _the_new_route
 	 * @return {void}
 	 * */
-	Router.add ('_triggerPopState', function (_params, route_name, _the_new_route, _config) {
+	Router.add('_triggerPopState', function (_params, route_name, _the_new_route, _config) {
 		//Set state in history
 		//Two times, for trigger "popstate"
 
-		if ( _config.trigger ) {
-			this.history.replaceState (_params, route_name, _the_new_route);
-			this.history.pushState (_params, route_name, _the_new_route);
-			this.history.back ();
+		if (_config.trigger) {
+			this.history.replaceState(_params, route_name, _the_new_route);
+			this.history.pushState(_params, route_name, _the_new_route);
+			this.history.back();
 		}
 
 	});
@@ -327,4 +349,4 @@
 	window.RouterClass = Router;
 
 
-}) (window);
+})(window);
