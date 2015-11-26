d82bad5b6d1a448c65bc240fb5c5c9253f04631c
gmena <gmjun2000@gmail.com>
2015-11-26 13:21:27 -0600

    Webpack

--- app/config/development/init.js
+++ app/config/development/init.js
@@ -13,27 +13,3 @@ exports.files = {
 		]
 	}
 };
-
-////Development
-//exports.files = {
-//	js: {
-//		output: 'dist/init',
-//		src   : [
-//			'system/base/Syrup',
-//			'system/base/Lib',
-//			'system/base/MiddleWare',
-//			'system/base/Observer',
-//			'system/base/Require',
-//			'system/base/Http',
-//			'system/base/Repo',
-//			'system/base/Workers',
-//			'system/base/View',
-//			'system/base/Model',
-//			'system/base/App',
-//			// Until here the core do not change
-//			// Add all the necessary libs from here
-//			'system/lib/Router',
-//			'system/lib/Hash'
-//		]
-//	}
-//};
