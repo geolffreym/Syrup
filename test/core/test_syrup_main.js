c606122077de1b18499cc1fce7d74775a98afbd9
gmena <gmjun2000@gmail.com>
2016-03-07 13:15:37 -0600

    //TODO escribir pruebas para confirmar que Core es injectable

--- test/core/test_syrup_main.js
+++ test/core/test_syrup_main.js
@@ -19,6 +19,11 @@ import D10sBuilder from '../../system/core/builder/D10sBuilder';
 import D10sComposite from '../../system/core/composite/D10sComposite';
 import Core from '../../system/core/Core';
 
+
+//Interface
+import Interface from '../../system/core/interface/Interface';
+import iD10sInjectable from '../../system/core/interface/iD10sInjectable';
+
 describe('Syrup.core', function () {
 
 	'use strict';
@@ -62,23 +67,20 @@ describe('Syrup.core', function () {
 
 	//The core
 	var _core = new Core();
-	_core.setD10s(_d10sCore);
 
-	//Custom matchers
+	//Custom matcher
 	beforeEach(function () {
 		jasmine.addMatchers({
 			toBeInstantiable: function (util, customEqualityTesters) {
 				return {
 					compare: function (Actual, expected) {
-						var result = {};
 						try {
 							new Actual;
-							result.pass = true;
-							return result;
-
+							return {pass: true};
 						} catch (e) {
-							result.message = 'Expected ' + expected + ' be an instantiable type';
-							return result;
+							return {
+								message: 'Expected ' + expected + ' be an instantiable type'
+							};
 						}
 					}
 				};
@@ -86,47 +88,52 @@ describe('Syrup.core', function () {
 		});
 	});
 
-	//TODO escribir pruebas para confirmar que Core es instanciable
 	//TODO escribir pruebas para confirmas que Core exige como parametro un tipo iCoreD10s /home/gmena/Documentos/syrup/system/core/interface/iD10s.js
 	it('should be instantiable', function () {
 		expect(Core).toBeInstantiable('CoreClass');
+		_core = new Core();
 	});
-	
-	//TODO escribir pruebas para confirmar que Core es injectable
+
 	it('should be iD10sInjectable', function () {
+		var _implement = function(){
+			Interface.implement(_core, iD10sInjectable);
+		};
+
+		expect(_implement).not.toThrow();
+		_core.setD10s(_d10sCore);
 
 	});
-	
+
 	it('should show version', function () {
 		expect(_core.VERSION).toBeDefined();
 		expect(_core.VERSION).toEqual(jasmine.any(String));
 	});
-	
+
 	it('should have the attribute "this.nav" and is Object type', function () {
 		expect(_core.nav).toBeDefined();
 		expect(_core.nav).toEqual(jasmine.any(Object));
 	});
-	
+
 	it('should have the attribute "this.is" and is Object type', function () {
 		expect(_core.is).toBeDefined();
 		expect(_core.is).toEqual(jasmine.any(Object));
 	});
-	
+
 	it('should have the attribute "this.m6s" and is Object type', function () {
 		expect(_core.m6s).toBeDefined();
 		expect(_core.m6s).toEqual(jasmine.any(Object));
 	});
-	
+
 	it('should have the attribute "this.u10s" and is Object type', function () {
 		expect(_core.u10s).toBeDefined();
 		expect(_core.u10s).toEqual(jasmine.any(Object));
 	});
-	
+
 	it('should have the attribute "this.$" and is Function type', function () {
 		expect(_core.$).toBeDefined();
 		expect(_core.$).toEqual(jasmine.any(Function));
 	});
-	
+
 });
 
 describe('Syrup.core.getNav()', function () {
@@ -188,15 +195,15 @@ describe('Syrup.core.getNav()', function () {
 		_core.getNav();
 		expect(_core.getNav).toHaveBeenCalled();
 	});
-	
+
 	it('should call (this.nav.local)', function () {
 		expect(_core.nav.local).toBeDefined();
 	});
-	
+
 	it('match type "object" in the return', function () {
 		expect(_core.getNav()).toEqual(jasmine.any(Object));
 	});
-	
+
 	it('match {nav}, {version} and {platform}, in returned object keys', function () {
 		expect(Object.keys(_core.getNav())).toEqual([
 			'nav', 'version', 'platform'
